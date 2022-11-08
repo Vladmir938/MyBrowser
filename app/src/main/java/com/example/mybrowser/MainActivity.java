d977431784b6c054b156de4f852bb138bcacef49
Vladmir938 <nikidvovamai@mail.ru>
2022-11-08 20:14:48 +0900

    init

---
+++ app/src/main/java/com/example/mybrowser/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mybrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webView);
+        myWebView.loadUrl("https://www.google.ru");
+        myWebView.setWebViewClient(new WebViewClient());
+
+        WebSettings myWebSettings = myWebView.getSettings();
+        myWebSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
